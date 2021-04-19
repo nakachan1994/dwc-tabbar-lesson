cdd37edf0125628f889c6619ddd4bcfb610c7e4d
yunakamura <nakachaaaan@gmail.com>
2021-04-19 10:48:57 +0900

    [finish]5-11

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-$('#tab-contents.tab[id != "tab"]').hide();
+$('#tab-contents .tab[id != "tab1"]').hide();
 
-$('#tab-menu a')
+$('#tab-menu a').on('click',function(event){
+  $('#tab-contents .tab').hide();
+  $('#tab-menu .active').removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
+});
